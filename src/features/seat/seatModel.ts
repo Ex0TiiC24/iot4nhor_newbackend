67d2da916f5e9944129f8726b603ae923ccd963f
Taiyo yamamoto <120293524+Ex0TiiC24@users.noreply.github.com>
2025-07-17 10:52:32 +0700

    Update seatModel.ts change limit query

--- src/features/seat/seatModel.ts
+++ src/features/seat/seatModel.ts
@@ -2,6 +2,6 @@ import { query } from "../../utils/database"
 
 export const queryAllSeat = async () =>{
     const result = await query`SELECT * FROM "floor2SIT"
-ORDER BY id DESC LIMIT 10;`
+ORDER BY id DESC LIMIT 500;`
     return result
 }
